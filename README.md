a981a385ee0da9c70eda50ab3a71c1610eb22d58
Rit <ritfreeservice@gmail.com>
2026-01-28 01:24:33 +0530

    1/28/2026

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ML-In-Production
-Understanding lifecycle of machine learning in production. Aim is not do well on metrics but instead understand the process of development and delopyment
+Understanding lifecycle of machine learning in production. Aim is not do well on metrics but instead understand the process of development and delopyment.
+MLOps Tool : Evidently.Ai
---
+++ template.py
@@ -0,0 +1,69 @@
+import os
+from pathlib import Path
+
+project_name = "Project"
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_validation.py",
+
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/constants/__init__.py",
+
+    f"{project_name}/entity/__init__.py",
+
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+
+    f"{project_name}/pipeline/__init__.py",
+
+    f"{project_name}/utils/__init__.py", 
+
+]
+"""
+    By placing an __init__.py file in every folder, you are telling Python to treat 
+    these directories as packages. This allows you to import your components later 
+    using syntax like: from project_name.components import model_trainer.
+"""
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+    else:
+        print(f"File is already present at {filepath}")
+
+"""
+Loop Breakdown :-
+
+---------------------------
+* filepath = Path(filepath)
+* filedir, filename = os.path.split(filepath)
+
+intially the filepath is something like: Bazza/ML-In-Production/__init__.py
+it break its into --> 1) filedir = Bazza/ML-In-Production
+                      2) filename = __init__.py
+---------------------------
+* if filedir != "":
+*    os.makedirs(filedir, exist_ok=True)
+
+checks if file path is mentioned and if so it os.makedirs makes that folder.
+exist_ok checks if it already exist and and prevents the code from crashing.
+----------------------------
+*if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+*    with open(filepath, w) as f:
+*        pass
+
+this code does 2 things, if file doesnt exist then creates it and if it exist but is empty(size=0) then recrease it.
+line 2 and 3 are easy way to create an empty file without passing text through it.
+----------------------------
+"""
+
+
+
